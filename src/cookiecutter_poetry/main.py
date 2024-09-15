affa3576ab5c3c3e8d6b6316a5fcc0578a84b14e
caerulescens <caerulescens.github@proton.me>
2024-09-15 14:38:03 -0400

    chore: packaging
    Passing `pytest`, `pre-commit`, `mypy`, `ruff`

---
+++ src/cookiecutter_poetry/main.py
@@ -0,0 +1,10 @@
+async def main(text: str) -> int:
+    """
+    Run the main function.
+
+    :param text:
+    :return:
+    """
+    exit_code = 0
+    print(text)
+    return exit_code
---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+from cookiecutter_poetry.main import main
+
+
+async def test_main() -> None:
+    """
+    Tests the main function.
+
+    :return: None
+    """
+    # setup
+    text = "Hello, World!"
+    exit_code = 0
+
+    # test
+    result = await main(text=text)
+
+    # assert
+    assert result == exit_code
